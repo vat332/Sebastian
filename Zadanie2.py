154f1dc00b8e49b6332c0820101e45eb4922b13e
vat332 <oporowymakus21@gmail.com>
2020-04-04 18:44:13 +0200

    vol 3

--- Zadanie2.py
+++ Zadanie2.py
@@ -1,3 +1,5 @@
+import random
+from random import randint 
 macierz = [
     [1, 2, 3, 1],
     [4, 5, 6, 2],
@@ -10,3 +12,16 @@ while y < 4:
     print(macierz[y][y])
     y +=1
 
+#for rzad in macierz:
+ #   for xd in rzad:
+  #      if()
+
+#plaska = [x for poziomo in macierz for x in poziomo]
+#print(plaska)
+
+N = 4
+list2 = [[random.randrange(-10, 10, 1) for i in range(4)] for j in range(4)]
+print(list2)
+print("liczby z przekatnej:\n")
+przekatna = [rzad[y] for y,rzad in enumerate(list2)]
+print(przekatna)
