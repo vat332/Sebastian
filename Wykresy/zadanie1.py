973bd1448c4df0937f2a947ad566da88d5619e03
vat332 <oporowymakus21@gmail.com>
2020-06-05 15:02:21 +0200

    Nowy git

---
+++ Wykresy/zadanie1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2, 100)
+
+# wykresy mogą być też dodawane do płótna definicja po definicji zamiast w pojedynczym wywołaniu funkcji plot()
+# tutaj użyty został również parametr label, który określa etykietę danego wykresu w legendzie
+plt.plot(x, x, label='liniowa')
+plt.plot(x, x**2, label='kwadratowa')
+plt.plot(x, x**3, label='sześcienna')
